63c73dc6abfb6b25724d3864badb6f6bb5adfac4
NachoDRT <iderodrigo@comillas.edu>
2023-11-13 15:19:58 +0100

    Add New Feature: VIdeo Stream

---
+++ test.py
@@ -0,0 +1,20 @@
+import cv2
+from picamera2 import Picamera2
+
+def stream_video():
+    picam = Picamera2()
+    picam.preview_configuration.main.size=(1280, 720)
+    picam.preview_configuration.main.format="RGB888"
+    picam.preview_configuration.align()
+    picam.configure("preview")
+    picam.start()
+
+    while True:
+        frame = picam.capture_array()
+        cv2.imshow("picam", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    stream_video()
